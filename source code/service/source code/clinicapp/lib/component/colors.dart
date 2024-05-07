5654829837ed148dbf4182e18707b7b73b57ea4e
Your Name <you@example.com>
2024-05-07 11:26:22 +0700

    false data

--- source code/service/source code/clinicapp/lib/component/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static final Color mainColor = const Color(0xFF407CE2);
-  static final Color whiteColor = const Color(0xFFFFFFFF);
-}
--- source code/service/source code/service/model/request/auth.request.go
+++
@@ -1,6 +0,0 @@
-package request
-
-type LoginRequest struct {
-	Username string `json:"username" validate:"required"`
-	Password string `json:"password" validate:"required"`
-}
